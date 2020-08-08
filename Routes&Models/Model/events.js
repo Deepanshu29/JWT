dff1ad7e942a39412a9d15ba8fe7534b318666c5
Aditi Aggarwal <thedeepanshuaggarwal@gmail.com>
2020-08-08 11:38:50 +0530

    jwt

--- Routes&Models/Model/events.js
+++ Routes&Models/Model/events.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/RegisterUser';
+const url = 'mongodb+srv://Deepanshu:X35RiRFRLREH1O70@cluster0-7m7a4.mongodb.net/RegisterUser?retryWrites=true&w=majority';
 mongoose.connect(url, {useNewUrlParser:true,useUnifiedTopology:true});
 const db = mongoose.connection;
 db.on('connected',()=>{
